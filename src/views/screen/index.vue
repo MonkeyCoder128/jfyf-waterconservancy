218d651df4997e2c6352ce1a019788fcae7eeb28
@sir-zhang <1529239705@qq.com>
2022-04-22 15:24:12 +0800

    框架整改

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,9 +1,8 @@
+
 <template>
   <el-row :gutter="12">
-  <el-col :span="24">
-    <el-card shadow="always">
-      首页
-    </el-card>
-  </el-col>
-</el-row>
+    <el-col :span="24">
+      <el-card shadow="always"> 首页 </el-card>
+    </el-col>
+  </el-row>
 </template>
