7db11e2f42e9ab9d062b0af9e0df3d1cda5c9071
huiruijiao <hrj@jfyf.tech>
2022-05-20 17:38:47 +0800

    fix: 视频监控

---
+++ src/views/remote/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="videoStyle" ref="init">
+      <video id="video-container">
+      </video>
+    </div>
+</template>
+<script>
+import  EZUIKit from 'ezuikit-js';
+export default {
+  data () {
+    return {
+     
+    };
+  },
+  mounted () {
+    let height= this.$refs.init.offsetHeight;
+    let width= this.$refs.init.offsetWidth;
+    // this.currentTime(); // 计时器
+    let that = this
+    fetch('https://open.ys7.com/jssdk/ezopen/demo/token')
+      .then(response => response.json())
+      .then(res => {
+        var accessToken = res.data.accessToken;
+        var player = new EZUIKit.EZUIKitPlayer({
+          id: 'video-container', // 视频容器ID
+          accessToken: accessToken,
+          poster:"",
+          url: 'ezopen://open.ys7.com/G39444019/1.live',
+          template: 'simple', // simple - 极简版;standard-标准版;security - 安防版(预览回放);voice-语音版; theme-可配置主题；
+          // plugin: ['talk'],                       // 加载插件，talk-对讲
+          height: height,
+          width: width,
+          // handleSuccess:function(ress){
+          //   console.log('0-0-0-',ress)
+          // }
+        });
+      });
+  },
+  watch: {
+    filterText (val) {
+      this.$refs.tree.filter(val);
+    },
+  },
+ 
+
+  methods: {
+    change() {
+      player.stop();
+      // 切换为直播
+      player.play({
+        url:"ezopen://open.ys7.com/244640009/1.live"
+      })
+      // setTimeout(()=>{
+      //   player.play({
+      //     url:"ezopen://open.ys7.com/244640009/1.live"
+      //   })      
+      // },1000)
+    },
+    
+  },
+};
+</script>
+<style scoped lang="scss">
+.videoStyle{
+  height: 100%;
+  width: 100%;
+}
+
+</style>
